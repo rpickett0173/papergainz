bb013bcbc438e21f10178853a03b2452c74a4640
rpickett0173 <56699145+rpickett0173@users.noreply.github.com>
2021-04-17 15:01:41 -0500

    Revert "Revert "Revert "test"""
    This reverts commit 054dcc164488c6daa004b0607a26bdff4ea980a7.

--- home/signUp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-#sign UP
-
-urlpatterns = [
-    path('home/Sign Up.html', views.signUp, name='signUp')
-]
--- home/signUp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-#signUp
-
-def signUp(request):
-    return render(request, 'Sign Up.html')
