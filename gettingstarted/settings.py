0500a3af1db934b73d1641607a829a5e2b38aa37
rpickett0173 <56699145+rpickett0173@users.noreply.github.com>
2021-04-22 10:55:20 -0500

    Undo image changes

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -113,9 +113,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR.child('media')
-MEDIA_URL = '/media/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
--- hello/urls.py
+++ hello/urls.py
@@ -1,9 +1,3 @@
-from django.conf import settings
-from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
-from gallery.core.views import *
-from django.contrib import admin
-
 from django.urls import path
 from . import views
 #home
@@ -11,12 +5,3 @@ urlpatterns = [
     path('Home.html', views.hello, name='hello'),
     path('Sign.html', views.signUp, name='signUp')
 ]
-
-urlpatterns = patterns(
-        path('Home.html', views.hello, name='hello'),
-        path('Sign.html', views.signUp, name='signUp')
-    'gallery.core.views',
-    url(r'^$', 'home', name='home'),
-    url(r'^gallery/$', GalleryList.as_view(), name='gallery_list'),
-    url(r'^admin/', include(admin.site.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
