91ab7d28af044e35bb765e6aa49e96d052acc34b
rpickett0173 <56699145+rpickett0173@users.noreply.github.com>
2021-04-22 11:02:56 -0500

    image t 1

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -112,6 +112,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+# MEDIA_ROOT = '<your_path>/media'
+# MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
