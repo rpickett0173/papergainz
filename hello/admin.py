75c773b431cd85cb356f8757d36a7018ba910be7
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-04-30 20:36:59 -0500

    Revert "test func"
    This reverts commit ced881e7bc949f831f4e15b3acb5746bf2445100.

--- hello/admin.py
+++ hello/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
-from .models import *
-
-admin.site.register(Users)
-admin.site.register(Games)
-admin.site.register(Rewards)
-admin.site.register(Bets)
-admin.site.register(DotaPlayerRanking)
+# Register your models here.
--- hello/apps.py
+++ hello/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hello'
-
-    def ready(self):
-        print("starting scheduler ...")
-        from .Scheduler import updater
-        updater.start()
