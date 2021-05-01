e0769c92eb26ded1a621649ece26fefaad05356f
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-04-30 20:44:41 -0500

    Revert "heroku edit"
    This reverts commit bdbf9495f26b82891b7729f505e6ffc9c4d2a56f.

--- hello/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from hello.models import Bets
-
-# function for creating the entity that displays the game information
-# and facilitates placing the bet
-class BetForm(forms.ModelForm):
-    class Meta:
-        model = Bets
-        fields = ['game_ID','team_bet','bet_amount']
