58e1bf6224a3d6368658f7a09b480dcef5c7d5db
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-04-30 21:11:57 -0500

    t

---
+++ hello/Scheduler/updater.py
@@ -0,0 +1,12 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from hello.views import GameData_Handler
+
+def start():
+    print("Started")
+    scheduler = BackgroundScheduler()
+    gamedata = GameData_Handler()
+    #scheduler.add_job(gamedata.DotaRank,"interval", minutes=1, id="game_collector_001", replace_existing=True)
+    scheduler.add_job(gamedata.get_api_data,"cron",hour=14 ,minute=23,second=30, id="API_job", replace_existing=True)
+    #scheduler.add_job(gamedata.calculate_payout_esport,"cron",hour=23 ,minute=59,second=59, id="Payout_job", replace_existing=True)
+
+    scheduler.start()
--- hello/models.py
+++ hello/models.py
@@ -1,61 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Greeting(models.Model):
+    when = models.DateTimeField("date created", auto_now_add=True)
+
 class Users(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    balance = models.PositiveIntegerField(null=True)
+    username = models.CharField(max_length=300)
+    password = models.CharField(max_length=300)
     def __str__(self):
         return self.username
-
-class Games(models.Model):
-    name = models.CharField(max_length=300)
-    match_id = models.PositiveIntegerField(null=True)
-    sport = models.CharField(max_length=300)
-    time_data = models.DateTimeField()
-    team1 = models.CharField(max_length=300)
-    team2 = models.CharField(max_length=300)
-    winner = models.CharField(max_length=300)
-    def __str__(self):
-        return self.name
-
-class Rewards(models.Model):
-    name = models.CharField(max_length=300)
-    price = models.PositiveSmallIntegerField()
-    reward_type = models.CharField(max_length=300)
-    def __str__(self):
-        return self.name
-
-# Bets model records each and every bet placed, the associated user and game IDs,
-# the date it was placed, details on the game (team1, team2), the team that
-# the user bet on, the size of the bet, and the result of their bet (win=1, loss=0)
-# and how much they won/loss.
-class Bets(models.Model):
-    user_ID = models.PositiveIntegerField()
-    game_ID = models.PositiveIntegerField(null=True) # null=true allows for this value to be null in the db
-    team1 = models.CharField(max_length=300, null=True)
-    team2 = models.CharField(max_length=300, null=True)
-    team_bet = models.CharField(max_length=300,null=True)
-    date_placed = models.DateTimeField(auto_now=True) #auto_now automatically makes this value the current time when the method is called
-    bet_amount = models.PositiveIntegerField()
-    result = models.BooleanField(null=True)
-    winloss = models.IntegerField(null=True)
-    def __str__(self):
-        return str(self.user_ID)
-
-
-class Odds(models.Model):
-    game_ID = models.PositiveIntegerField()
-    team1_odds = models.DecimalField(max_digits=3, decimal_places=2)
-    team2_odds = models.DecimalField(max_digits=3, decimal_places=2)
-    def __str__(self):
-        return self.game_ID
-
-
-class DotaPlayerRanking(models.Model):
-    name = models.CharField(max_length=300)
-    rank = models.PositiveIntegerField(blank=True, null=True)
-    steamid = models.TextField(null=True)
-    avatar = models.TextField(blank=True)
-    def __str__(self):
-        return self.name
