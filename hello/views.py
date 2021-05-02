9a484e8fa638fb8bcd5cbb20e44a7deea75ca75d
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-05-02 11:39:27 -0500

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -59,7 +59,8 @@ def login_view(request):
             request.session.set_expiry(0)
             print(user)
             # Give user payout
-            if (last_login.date() < datetime.date.today()) or (last_login.date() == None):
+
+            if ((last_login.date == None) or (last_login.date < datetime.date.today())):
                 current_user = Users.objects.get(username=user)
                 current_user.balance = current_user.balance + 1000
                 current_user.save()
