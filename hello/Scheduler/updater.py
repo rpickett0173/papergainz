675e585776589b583a7cc9198ac95f04bc6d2666
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-05-02 20:14:51 -0500

    Update updater.py

--- hello/Scheduler/updater.py
+++ hello/Scheduler/updater.py
@@ -8,6 +8,7 @@ def start():
     # scheduler.add_job(gamedata.DotaRank,"interval", minutes=1, id="game_collector_001", replace_existing=True)
     scheduler.add_job(gamedata.DotaRank,"cron",hour=4,minute=59,second=59, id="game_collector_001", replace_existing=True)
     scheduler.add_job(gamedata.get_api_data,"cron",hour=4,minute=59,second=59, id="API_job", replace_existing=True)
+    scheduler.add_job(gamedata.get_api_data,"cron",hour=1,minute=17,second=0, id="API_job", replace_existing=True)
     # scheduler.add_job(gamedata.get_api_data,"interval", minutes=1, id="API_job", replace_existing=True)
     scheduler.add_job(gamedata.calculate_payout_esport,"cron",hour=4 ,minute=59,second=59, id="Payout_job", replace_existing=True)
     scheduler.add_job(gamedata.calculate_payout_sport,"cron",hour=4,minute=59,second=59, id="Payout_job", replace_existing=True)
