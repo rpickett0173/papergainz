21144b14508436d8b273bbb3831a6571fd3f9fab
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-05-04 20:39:52 -0500

    Update updater.py

--- hello/Scheduler/updater.py
+++ hello/Scheduler/updater.py
@@ -5,12 +5,16 @@ def start():
     print("Started")
     scheduler = BackgroundScheduler()
     gamedata = GameData_Handler()
-    # scheduler.add_job(gamedata.DotaRank,"interval", minutes=1, id="game_collector_001", replace_existing=True)
-    scheduler.add_job(gamedata.DotaRank,"cron",hour=4,minute=59,second=59, id="game_collector_001", replace_existing=True)
-    scheduler.add_job(gamedata.get_api_data,"cron",hour=4,minute=59,second=59, id="API_job", replace_existing=True)
-    # scheduler.add_job(gamedata.get_api_data,"cron",hour=1,minute=17,second=0, id="API_job", replace_existing=True)
-    # scheduler.add_job(gamedata.get_api_data,"interval", minutes=1, id="API_job", replace_existing=True)
-    scheduler.add_job(gamedata.calculate_payout_esport,"cron",hour=4 ,minute=59,second=59, id="Payout_job", replace_existing=True)
-    scheduler.add_job(gamedata.calculate_payout_sport,"cron",hour=4,minute=59,second=59, id="Payout_job", replace_existing=True)
+    # Cron timers
+    # scheduler.add_job(gamedata.DotaRank,"cron",hour=4,minute=59,second=59, id="game_collector_001", replace_existing=True)
+    # scheduler.add_job(gamedata.get_api_data,"cron",hour=4,minute=59,second=59, id="API_job", replace_existing=True)
+    # scheduler.add_job(gamedata.calculate_payout_esport,"cron",hour=4 ,minute=59,second=59, id="Payout_job", replace_existing=True)
+    # scheduler.add_job(gamedata.calculate_payout_sport,"cron",hour=4,minute=59,second=59, id="Payout_job", replace_existing=True)
+
+    # interval timers
+    scheduler.add_job(gamedata.DotaRank,"interval", minutes=60, id="game_collector_001", replace_existing=True)
+    scheduler.add_job(gamedata.get_api_data,"interval", minutes=60, id="API_job", replace_existing=True)
+    scheduler.add_job(gamedata.calculate_payout_esport,"interval", minutes=60, id="Payout_job", replace_existing=True)
+    scheduler.add_job(gamedata.calculate_payout_sport,"interval", minutes=60, id="Payout_job", replace_existing=True)
 
     scheduler.start()
